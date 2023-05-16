6bead94577360bb803f36fa567abed4023c5a512
Anna Hrulckova <annahrulkova@yandex.ru>
2023-05-16 17:49:46 +0400

    не работает запрос

--- src/components/Authorization.js
+++ src/components/Authorization.js
@@ -1,5 +1,34 @@
 import './Authorization.css';
 export const Authorization = () => {
+	const han = (e) => {
+		e.preventDefault();
+		const idInstance = '1101821223';
+		const apiTokenInstance = '758ada212e094a7883a6ecadf6c3b9e9c6ea1bc713324d8682';
+
+		fetch('https://api.green-api.com', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				idInstance,
+				apiTokenInstance,
+			}),
+		})
+			.then((response) => {
+				if (response.ok) {
+					console.log('ok');
+					return response.json();
+				} else {
+					console.log('error');
+					throw new Error('Authorization failed');
+				}
+			})
+			.then((data) => {})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
 	return (
 		<main className='main'>
 			<form className='main__form form'>
@@ -11,7 +40,7 @@ export const Authorization = () => {
 					autoComplete='off'
 					placeholder='apiTokenInstance'
 				></input>
-				<button type='submite' className='form__btn'>
+				<button onClick={han} type='submite' className='form__btn'>
 					Войти
 				</button>
 			</form>
