38737f8d2b75f3bc01780a5c18c9d98da2a7f135
Anna Hrulckova <annahrulkova@yandex.ru>
2023-05-17 22:50:54 +0400

    стилизованы сообщения

--- src/components/Messenger.js
+++ src/components/Messenger.js
@@ -1,12 +1,33 @@
 import './Messenger.css';
 import { useState } from 'react';
+
 export const Messenger = () => {
 	const idInstance = localStorage.getItem('idInstance');
 	const apiTokenInstance = localStorage.getItem('apiTokenInstance');
 	const phoneNum = localStorage.getItem('phoneNum');
 	const phoneFormat = phoneNum + '@c.us';
+
 	const [message, setMessage] = useState('');
-	const hadlerr = (e) => {
+	const [messages, setMessages] = useState([]);
+
+	fetch(`https://api.green-api.com/waInstance${idInstance}/ReceiveNotification/${apiTokenInstance}`, {
+		method: 'GET',
+	})
+		.then((response) => {
+			console.log(response);
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+
+	fetch(`https://api.green-api.com/waInstance${idInstance}/DeleteNotification/${apiTokenInstance}`, {
+		method: 'DELETE',
+		redirect: 'follow',
+	}).then((response) => {
+		console.log(response);
+	});
+
+	const handleSend = (e) => {
 		e.preventDefault();
 		fetch(`https://api.green-api.com/waInstance${idInstance}/SendMessage/${apiTokenInstance}`, {
 			method: 'POST',
@@ -18,6 +39,9 @@ export const Messenger = () => {
 			.then((response) => {
 				if (response.ok) {
 					console.log(response);
+					setMessages([...messages, message]);
+					setMessage('');
+					console.log(messages);
 				} else {
 					console.log('error');
 				}
@@ -26,21 +50,23 @@ export const Messenger = () => {
 				console.log(error);
 			});
 	};
+
 	return (
 		<main className='main'>
 			<div className='substrate'></div>
 			<div className='main__pnone'>
 				<h4 className='main__number'>{phoneNum}</h4>
 			</div>
-			<div className='main__messages'></div>
-			<form className='main__message-form '>
-				<div className='main__message' contentEditable='true' onInput={(e) => setMessage(e.target.innerText)}></div>
-				<button
-					onClick={(e) => {
-						hadlerr(e);
-					}}
-					className='main__btn'
-				>
+			<div className='main__messages messages'>
+				{messages.map((msg, index) => (
+					<p key={index} className='messages__message'>
+						{msg}
+					</p>
+				))}
+			</div>
+			<form className='main__message-form'>
+				<textarea className='main__message' value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
+				<button onClick={handleSend} className='main__btn'>
 					<img className='main__send' src='send-btn.png' alt='отправить'></img>
 				</button>
 			</form>
